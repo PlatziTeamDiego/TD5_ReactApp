011bbe8407f4376958a0aca9c59c5ea083d562b3
Erik Guevara <emgladino@gmail.com>
2020-09-05 11:33:57 -0500

    Ajuste de front end
    Se creo la funcion para que las imagenes quedaran aleatorias segun corresponda por cada persona del equipo, se ajusto el css para los puntos de redimencion.

--- src/imgrandom.js
+++ src/imgrandom.js
@@ -1,5 +1,5 @@
 export function skillAvatar() {
-    const power = ["power-12", "Superhero-01", "Superhero-02", "Superhero02-01", "Superhero02-02", "Superhero02-04", "Superhero-03", "Superhero-04"];
+    const power = ["Superhero-01", "Superhero-02", "Superhero02-01", "Superhero02-02", "Superhero02-03", "Superhero02-04", "Superhero-03", "Superhero-04"];
     const weakness = ["GermsCharacter1-01", "GermsCharacter1-02", "GermsCharacter1-03", "GermsCharacter1-04", "monster-01", "monster-02", "monster-03"];
     let images = [];
     images.push("./src/assets/images/" + power[numberAll(power.length)] + ".png");
